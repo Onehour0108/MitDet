d905fd2da3bc3b5152c18b16421ac8b6025c38b9
Onehour <106423830+Onehour0108@users.noreply.github.com>
2023-06-05 10:32:56 +0800

    Add files via upload

---
+++ network/resnet38d_dassl.py
@@ -0,0 +1,429 @@
+import torch
+from torch import nn
+import numpy as np
+import pdb
+import random
+import torch.nn.functional as F
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, stride=1, first_dilation=None, dilation=1):
+        super(ResBlock, self).__init__()
+
+        self.same_shape = (in_channels == out_channels and stride == 1)
+
+        if first_dilation == None: first_dilation = dilation
+
+        self.bn_branch2a = nn.BatchNorm2d(in_channels)
+
+        self.conv_branch2a = nn.Conv2d(in_channels, mid_channels, 3, stride,
+                                       padding=first_dilation, dilation=first_dilation, bias=False)
+
+        self.bn_branch2b1 = nn.BatchNorm2d(mid_channels)
+
+        self.conv_branch2b1 = nn.Conv2d(mid_channels, out_channels, 3, padding=dilation, dilation=dilation, bias=False)
+
+        if not self.same_shape:
+            self.conv_branch1 = nn.Conv2d(in_channels, out_channels, 1, stride, bias=False)
+
+    def forward(self, x, get_x_bn_relu=False):
+        # pdb.set_trace()
+
+        branch2 = self.bn_branch2a(x)
+        branch2 = F.relu(branch2)
+
+        x_bn_relu = branch2
+
+        if not self.same_shape:
+            branch1 = self.conv_branch1(branch2)
+        else:
+            branch1 = x
+
+        branch2 = self.conv_branch2a(branch2)
+        branch2 = self.bn_branch2b1(branch2)
+        branch2 = F.relu(branch2)
+        branch2 = self.conv_branch2b1(branch2)
+
+        x = branch1 + branch2
+
+        if get_x_bn_relu:
+            return x, x_bn_relu
+
+        return x
+
+    def __call__(self, x, get_x_bn_relu=False):
+        return self.forward(x, get_x_bn_relu=get_x_bn_relu)
+
+class ResBlock_bot(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, dilation=1, dropout=0.):
+        super(ResBlock_bot, self).__init__()
+
+        self.same_shape = (in_channels == out_channels and stride == 1)
+
+        self.bn_branch2a = nn.BatchNorm2d(in_channels)
+        self.conv_branch2a = nn.Conv2d(in_channels, out_channels//4, 1, stride, bias=False)
+
+        self.bn_branch2b1 = nn.BatchNorm2d(out_channels//4)
+        self.dropout_2b1 = torch.nn.Dropout2d(dropout)
+        self.conv_branch2b1 = nn.Conv2d(out_channels//4, out_channels//2, 3, padding=dilation, dilation=dilation, bias=False)
+
+        self.bn_branch2b2 = nn.BatchNorm2d(out_channels//2)
+        self.dropout_2b2 = torch.nn.Dropout2d(dropout)
+        self.conv_branch2b2 = nn.Conv2d(out_channels//2, out_channels, 1, bias=False)
+
+        if not self.same_shape:
+            self.conv_branch1 = nn.Conv2d(in_channels, out_channels, 1, stride, bias=False)
+
+    def forward(self, x, get_x_bn_relu=False):
+        # pdb.set_trace()
+
+        branch2 = self.bn_branch2a(x)
+        branch2 = F.relu(branch2)
+        x_bn_relu = branch2
+
+        branch1 = self.conv_branch1(branch2)
+
+        branch2 = self.conv_branch2a(branch2)
+
+        branch2 = self.bn_branch2b1(branch2)
+        branch2 = F.relu(branch2)
+        branch2 = self.dropout_2b1(branch2)
+        branch2 = self.conv_branch2b1(branch2)
+
+        branch2 = self.bn_branch2b2(branch2)
+        branch2 = F.relu(branch2)
+        branch2 = self.dropout_2b2(branch2)
+        branch2 = self.conv_branch2b2(branch2)
+
+        x = branch1 + branch2
+
+        if get_x_bn_relu:
+            return x, x_bn_relu
+
+        return x
+
+    def __call__(self, x, get_x_bn_relu=False):
+        return self.forward(x, get_x_bn_relu=get_x_bn_relu)
+
+class Normalize():
+    def __init__(self, mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225)):
+
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, img):
+        imgarr = np.asarray(img)
+        proc_img = np.empty_like(imgarr, np.float32)
+
+        proc_img[..., 0] = (imgarr[..., 0] / 255. - self.mean[0]) / self.std[0]
+        proc_img[..., 1] = (imgarr[..., 1] / 255. - self.mean[1]) / self.std[1]
+        proc_img[..., 2] = (imgarr[..., 2] / 255. - self.mean[2]) / self.std[2]
+
+        return proc_img
+
+class MixStyle(nn.Module):
+    """MixStyle.
+    Reference:
+      Zhou et al. Domain Generalization with MixStyle. ICLR 2021.
+    """
+
+    def __init__(self, p=0.5, alpha=0.1, eps=1e-6, mix="random"):
+        """
+        Args:
+          p (float): probability of using MixStyle.
+          alpha (float): parameter of the Beta distribution.
+          eps (float): scaling parameter to avoid numerical issues.
+          mix (str): how to mix.
+        """
+        super().__init__()
+        self.p = p
+        self.beta = torch.distributions.Beta(alpha, alpha)
+        self.eps = eps
+        self.alpha = alpha
+        self.mix = mix
+        self._activated = True
+
+    def __repr__(self):
+        return (
+            f"MixStyle(p={self.p}, alpha={self.alpha}, eps={self.eps}, mix={self.mix})"
+        )
+
+    def set_activation_status(self, status=True):
+        self._activated = status
+
+    def update_mix_method(self, mix="random"):
+        self.mix = mix
+
+    def forward(self, x):
+        if not self.training or not self._activated:
+            return x
+
+        if random.random() > self.p:
+            return x
+
+        B = x.size(0)
+
+        mu = x.mean(dim=[2, 3], keepdim=True)
+        var = x.var(dim=[2, 3], keepdim=True)
+        sig = (var + self.eps).sqrt()
+        mu, sig = mu.detach(), sig.detach()
+        x_normed = (x-mu) / sig
+
+        lmda = self.beta.sample((B, 1, 1, 1))
+        lmda = lmda.to(x.device)
+
+        if self.mix == "random":
+            # random shuffle
+            perm = torch.randperm(B)
+
+        elif self.mix == "crossdomain":
+            # split into two halves and swap the order
+            perm = torch.arange(B - 1, -1, -1)  # inverse index
+            perm_b, perm_a = perm.chunk(2)
+            perm_b = perm_b[torch.randperm(perm_b.shape[0])]
+            perm_a = perm_a[torch.randperm(perm_a.shape[0])]
+            perm = torch.cat([perm_b, perm_a], 0)
+
+        else:
+            raise NotImplementedError
+
+        mu2, sig2 = mu[perm], sig[perm]
+        mu_mix = mu*lmda + mu2 * (1-lmda)
+        sig_mix = sig*lmda + sig2 * (1-lmda)
+
+        return x_normed*sig_mix + mu_mix
+
+class EFDMix(nn.Module):
+    """EFDMix.
+    Reference:
+      Zhang et al. Exact Feature Distribution Matching for Arbitrary Style Transfer and Domain Generalization. CVPR 2022.
+    """
+
+    def __init__(self, p=0.5, alpha=0.1, eps=1e-6, mix="random"):
+        """
+        Args:
+          p (float): probability of using MixStyle.
+          alpha (float): parameter of the Beta distribution.
+          eps (float): scaling parameter to avoid numerical issues.
+          mix (str): how to mix.
+        """
+        super().__init__()
+        self.p = p
+        self.beta = torch.distributions.Beta(alpha, alpha)
+        self.eps = eps
+        self.alpha = alpha
+        self.mix = mix
+        self._activated = True
+
+    def __repr__(self):
+        return (
+            f"MixStyle(p={self.p}, alpha={self.alpha}, eps={self.eps}, mix={self.mix})"
+        )
+
+    def set_activation_status(self, status=True):
+        self._activated = status
+
+    def update_mix_method(self, mix="random"):
+        self.mix = mix
+
+    def forward(self, x):
+        if not self.training or not self._activated:
+            return x
+
+        if random.random() > self.p:
+            return x
+
+        B, C, W, H = x.size(0), x.size(1), x.size(2), x.size(3)
+        x_view = x.view(B, C, -1)
+        value_x, index_x = torch.sort(x_view)  # sort inputs
+        lmda = self.beta.sample((B, 1, 1))
+        lmda = lmda.to(x.device)
+
+        if self.mix == "random":
+            # random shuffle
+            perm = torch.randperm(B)
+
+        elif self.mix == "crossdomain":
+            # split into two halves and swap the order
+            perm = torch.arange(B - 1, -1, -1)  # inverse index
+            perm_b, perm_a = perm.chunk(2)
+            perm_b = perm_b[torch.randperm(perm_b.shape[0])]
+            perm_a = perm_a[torch.randperm(perm_a.shape[0])]
+            perm = torch.cat([perm_b, perm_a], 0)
+
+        else:
+            raise NotImplementedError
+
+        inverse_index = index_x.argsort(-1)
+        x_view_copy = value_x[perm].gather(-1, inverse_index)
+        new_x = x_view + (x_view_copy - x_view.detach()) * (1-lmda)
+        return new_x.view(B, C, W, H)
+    
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # self.mixstyle = MixStyle(p=0.5, alpha=0.1)
+        self.efdmix=EFDMix(p=0.5, alpha=0.1)
+        self.conv1a = nn.Conv2d(3, 64, 3, padding=1, bias=False) #224
+
+        self.efdmix_1=EFDMix(p=0.5, alpha=0.1)
+
+        self.b2 = ResBlock(64, 128, 128, stride=2)#112
+        self.b2_1 = ResBlock(128, 128, 128)
+        self.b2_2 = ResBlock(128, 128, 128)
+
+        self.efdmix_2=EFDMix(p=0.5, alpha=0.1)
+
+        self.b3 = ResBlock(128, 256, 256, stride=2)#56
+        self.b3_1 = ResBlock(256, 256, 256)
+        self.b3_2 = ResBlock(256, 256, 256)
+
+        self.efdmix_3=EFDMix(p=0.5, alpha=0.1)
+
+        self.b4 = ResBlock(256, 512, 512, stride=2)#28
+        self.b4_1 = ResBlock(512, 512, 512)
+        self.b4_2 = ResBlock(512, 512, 512)
+        self.b4_3 = ResBlock(512, 512, 512)
+        self.b4_4 = ResBlock(512, 512, 512)
+        self.b4_5 = ResBlock(512, 512, 512)
+
+        self.b5 = ResBlock(512, 512, 1024, stride=1, first_dilation=1, dilation=2)
+        self.b5_1 = ResBlock(1024, 512, 1024, dilation=2)
+        self.b5_2 = ResBlock(1024, 512, 1024, dilation=2)
+
+        # self.b6 = ResBlock_bot(1024, 2048, stride=1, dilation=4, dropout=0.3)
+        self.b6 = ResBlock_bot(1024, 2048, stride=1, dilation=4, dropout=0.5 )
+
+        # self.b7 = ResBlock_bot(2048, 4096, dilation=4, dropout=0.5)
+        self.b7 = ResBlock_bot(2048, 4096, dilation=4, dropout=0.5)
+
+        self.bn7 = nn.BatchNorm2d(4096)
+
+        self.not_training = []
+        
+
+        self.normalize = Normalize()
+
+        return
+
+    # def forward_(self, x):
+    #     x= self.forward_as_dict(x)['conv6']
+
+    def forward(self, x):
+        return self.forward_as_dict(x)['conv6']
+            
+    def forward_attention(self, x):
+        return self.forward_as_dict(x)['at_map']
+
+    def forward_as_dict(self, x):
+        # pdb.set_trace()
+
+        x = self.conv1a(x) # 224 224
+
+        x=self.efdmix(x)
+
+        x = self.b2(x)
+        x = self.b2_1(x)
+        x = self.b2_2(x) # 128 112 112
+
+        x=self.efdmix(x)
+
+        x = self.b3(x)
+        x = self.b3_1(x)
+        x = self.b3_2(x)
+
+        x=self.efdmix(x)
+
+        x = self.b4(x)
+        x = self.b4_1(x)
+        x = self.b4_2(x)
+        x = self.b4_3(x)
+        x = self.b4_4(x)
+        x = self.b4_5(x)
+
+
+        x, conv4 = self.b5(x, get_x_bn_relu=True) #x:[batch_size, 1024,28,28]  conv4:[batch_size, 512,28,28]
+        x = self.b5_1(x)
+        x = self.b5_2(x)
+
+        x, conv5 = self.b6(x, get_x_bn_relu=True) #x:[batch_size, 2048,28,28]  conv4:[batch_size, 1024,28,28]
+
+        x = self.b7(x) #x:[batch_size, 4096 ,28 ,28]
+        at_map = self.bn7(x)
+        conv6 = F.relu(self.bn7(x))
+        # pdb.set_trace()0
+
+        return dict({'conv4': conv4, 'conv5': conv5, 'conv6': conv6, 'at_map':at_map,'x':x})
+
+    def train(self, mode=True):
+
+        super().train(mode)
+
+        for layer in self.not_training:
+
+            if isinstance(layer, torch.nn.Conv2d):
+                layer.weight.requires_grad = False
+
+            elif isinstance(layer, torch.nn.Module):
+                for c in layer.children():
+                    c.weight.requires_grad = False
+                    if c.bias is not None:
+                        c.bias.requires_grad = False
+
+        for layer in self.modules():
+
+            if isinstance(layer, torch.nn.BatchNorm2d):
+                layer.eval()
+                layer.bias.requires_grad = False
+                layer.weight.requires_grad = False
+
+        return
+
+def convert_mxnet_to_torch(filename):
+    import mxnet
+
+    save_dict = mxnet.nd.load(filename)
+
+    renamed_dict = dict()
+
+    bn_param_mx_pt = {'beta': 'bias', 'gamma': 'weight', 'mean': 'running_mean', 'var': 'running_var'}
+
+    for k, v in save_dict.items():
+
+        v = torch.from_numpy(v.asnumpy())
+        toks = k.split('_')
+
+        if 'conv1a' in toks[0]:
+            renamed_dict['conv1a.weight'] = v
+
+        elif 'linear1000' in toks[0]:
+            pass
+
+        elif 'branch' in toks[1]:
+
+            pt_name = []
+
+            if toks[0][-1] != 'a':
+                pt_name.append('b' + toks[0][-3] + '_' + toks[0][-1])
+            else:
+                pt_name.append('b' + toks[0][-2])
+
+            if 'res' in toks[0]:
+                layer_type = 'conv'
+                last_name = 'weight'
+
+            else:  # 'bn' in toks[0]:
+                layer_type = 'bn'
+                last_name = bn_param_mx_pt[toks[-1]]
+
+            pt_name.append(layer_type + '_' + toks[1])
+
+            pt_name.append(last_name)
+
+            torch_name = '.'.join(pt_name)
+            renamed_dict[torch_name] = v
+
+        else:
+            last_name = bn_param_mx_pt[toks[-1]]
+            renamed_dict['bn7.' + last_name] = v
+
+    return renamed_dict
